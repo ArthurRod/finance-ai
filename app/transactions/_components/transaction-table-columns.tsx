99f732eef8ca6b1fb46c215e6cc48dc2949ba4d0
Arthur Rodrigues <arthur9713@hotmail.com>
2024-11-07 11:13:51 -0300

    feat: transaction table collumns formatted

--- app/transactions/_components/transaction-table-columns.tsx
+++ app/transactions/_components/transaction-table-columns.tsx
@@ -3,6 +3,12 @@
 import { Transaction } from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
 import { TransactionTypeBadge } from "./transaction-type-badge";
+import {
+  TRANSACTION_CATEGORY_LABELS,
+  TRANSACTION_PAYMENT_METHOD_LABELS,
+} from "@/app/_constants/transaction";
+import { Button } from "@/app/_components/ui/button";
+import { PencilIcon, TrashIcon } from "lucide-react";
 
 export const transactionTableColumns: ColumnDef<Transaction>[] = [
   {
@@ -19,21 +25,52 @@ export const transactionTableColumns: ColumnDef<Transaction>[] = [
   {
     accessorKey: "category",
     header: "Categoria",
-  },
-  {
-    accessorKey: "date",
-    header: "Data",
+    cell: ({ row: { original: transaction } }) =>
+      TRANSACTION_CATEGORY_LABELS[transaction.category],
   },
   {
     accessorKey: "amount",
     header: "Valor",
+    cell: ({ row: { original: transaction } }) =>
+      new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      }).format(Number(transaction.amount)),
+  },
+  {
+    accessorKey: "date",
+    header: "Data",
+    cell: ({ row: { original: transaction } }) =>
+      new Date(transaction.date).toLocaleDateString("pt-BR", {
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+      }),
   },
   {
     accessorKey: "paymentMethod",
     header: "Método de pagamento",
+    cell: ({ row: { original: transaction } }) =>
+      TRANSACTION_PAYMENT_METHOD_LABELS[transaction.paymentMethod],
   },
   {
     accessorKey: "actions",
     header: "Ações",
+    cell: () => {
+      return (
+        <>
+          <Button variant="ghost" size="icon" className="text-muted-foreground">
+            <PencilIcon />
+          </Button>
+          <Button
+            variant="ghost"
+            size="icon"
+            className="ml-1 text-muted-foreground"
+          >
+            <TrashIcon />
+          </Button>
+        </>
+      );
+    },
   },
 ];
